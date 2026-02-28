1e616ac58f743a07dd1c4f1e37ff8966013c5643
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:53:36 -0400

    tarea13

---
+++ tarea13/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var numeros = (1, 2, 3, 4, 5);
+
+        Console.WriteLine("Tercer elemento: " + numeros.Item3);
+    }
+}

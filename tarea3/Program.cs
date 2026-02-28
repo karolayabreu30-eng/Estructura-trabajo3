2702caac05bd6c597caf6f851d4dd8df97f2ce9b
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:28:54 -0400

    tarea3

---
+++ tarea3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (EsPar(num))
+            Console.WriteLine("Es par");
+        else
+            Console.WriteLine("Es impar");
+    }
+}

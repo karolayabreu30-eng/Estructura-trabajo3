c879e7092d7756f157fe084b636f77b2ad52d5b5
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:55:17 -0400

    tarea14

---
+++ tarea14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var frutas = ("Manzana", "Pera", "Uva");
+        var verduras = ("Zanahoria", "Lechuga", "Tomate");
+
+        var todo = (frutas.Item1, frutas.Item2, frutas.Item3,
+                    verduras.Item1, verduras.Item2, verduras.Item3);
+
+        Console.WriteLine(todo);
+    }
+}

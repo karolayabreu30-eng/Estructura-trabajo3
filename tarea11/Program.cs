1fe3dc9e15b3d1b96255ef5a37808b5dc14809f8
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:49:07 -0400

    tarea11

---
+++ tarea11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Coche
+{
+    public string color;
+
+    public string ObtenerColor()
+    {
+        return color;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Coche miCoche = new Coche();
+        miCoche.color = "Rojo";
+
+        Console.WriteLine("El color del coche es: " + miCoche.ObtenerColor());
+    }
+}

2673d913409cba4b7a755046d6c58e9ac56b3749
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:31:19 -0400

    tarea4

---
+++ tarea4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int Mayor(int a, int b)
+    {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El mayor es: " + Mayor(num1, num2));
+    }
+}

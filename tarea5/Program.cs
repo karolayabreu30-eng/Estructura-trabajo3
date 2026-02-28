b1bb078b6e19d4294e54682cc1dad877a13c2eff
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:33:32 -0400

    tarea5

---
+++ tarea5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static int ContarVocales(string texto)
+    {
+        int contador = 0;
+        texto = texto.ToLower();
+
+        foreach (char letra in texto)
+        {
+            if ("aeiou".Contains(letra))
+                contador++;
+        }
+
+        return contador;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese una palabra o frase: ");
+        string texto = Console.ReadLine();
+
+        Console.WriteLine("Cantidad de vocales: " + ContarVocales(texto));
+    }
+};

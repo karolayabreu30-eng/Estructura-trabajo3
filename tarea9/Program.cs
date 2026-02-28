24489ae5e11b258c93d9b6cc824c539a2d6e0c11
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:44:13 -0400

    tarea9

---
+++ tarea9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Persona
+{
+    public string nombre;
+
+    public void Saludar()
+    {
+        Console.WriteLine("Hola, soy " + nombre);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona p = new Persona();
+        p.nombre = "Carlos";
+        p.Saludar();
+    }
+}

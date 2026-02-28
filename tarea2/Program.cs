09a8114cd5e3a308d2fbaff3b424587eb22d4317
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:26:14 -0400

    tarea2

---
+++ tarea2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static double CalcularArea(double radio)
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el radio: ");
+        double radio = double.Parse(Console.ReadLine());
+
+        double area = CalcularArea(radio);
+        Console.WriteLine("El área es: " + area);
+    }
+}

c40a1ad55dc3df02a17edca4c19b96e5d89ff248
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:51:08 -0400

    tarea12

---
+++ tarea12/Program.cs
@@ -0,0 +1,19 @@
+﻿class Libro
+{
+    public string titulo;
+
+    public void MostrarTitulo()
+    {
+        Console.WriteLine("El título del libro es: " + titulo);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Libro libro1 = new Libro();
+        libro1.titulo = "Cien años de soledad";
+        libro1.MostrarTitulo();
+    }
+}

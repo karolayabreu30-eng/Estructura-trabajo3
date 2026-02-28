2c5b98699f6254678f3b2548f2199fba19630a47
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:23:52 -0400

    tarea1

---
+++ tarea1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int resultado = Sumar(num1, num2);
+        Console.WriteLine("La suma es: " + resultado);
+    }
+}

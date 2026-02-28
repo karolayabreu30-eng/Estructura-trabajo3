d91e768f5a1609db3a9ad7f875d53e2a62d7e417
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:42:43 -0400

    tarea8

---
+++ tarea8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static int LongitudCadena(string texto)
+    {
+        return texto.Length;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un texto: ");
+        string texto = Console.ReadLine();
+
+        Console.WriteLine("Longitud: " + LongitudCadena(texto));
+    }
+}

70545f2c3dc397adc65d3654b1f8ed7156049ffe
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-27 20:36:04 -0400

    tarea6

---
+++ tarea6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static double Convertir(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese grados Celsius: ");
+        double c = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("En Fahrenheit: " + Convertir(c));
+    }
+}
